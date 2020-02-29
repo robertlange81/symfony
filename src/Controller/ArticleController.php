b7d1fff72ac60fdd44934a42b7fedd4110862a9d
Robert Lange <robert.lange.81@gmail.com>
2020-03-01 00:58:49 +0100

    use newly required annotations for routing

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -5,9 +5,14 @@ namespace App\Controller;
 
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController
 {
+    /**
+     * @Route("/")
+     * @return Response
+     */
     public function homePage() {
         return new Response("Hello Symfony Respone Object");
     }
